ef045fc9e80e629b7700a8a9222b3644e39acd55
Rivas1 <rivas1@cpp.edu>
2019-02-25 22:09:40 -0800

    User validation + Addition of email_index in db
    email_index to eliminate two accidental requests from initial user sign up. Ensures no duplication occurs at the database level.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190226043933) do
+ActiveRecord::Schema.define(version: 20190226053234) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190226043933) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
