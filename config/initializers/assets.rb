436c1711d09aca65488889359544fb4a693114c4
Rivas1 <rivas1@cpp.edu>
2019-05-07 15:10:09 -0700

    Removed "data-turbolinks-track" stylesheet tag
    Removed from application.html.erb
    It was taking precedence over bootstrap's styling.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,6 +14,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 
-Rails.application.config.assets.precompile += %w( cover.css signin.css )
+Rails.application.config.assets.precompile += %w( cover.scss signin.scss )
 
 
